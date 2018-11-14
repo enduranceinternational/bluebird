b6c9a9ba1b7f75416d4e6967a546d9572d51b450
jordanwunderlich <jordan.wunder@gmail.com>
2018-11-14 15:09:21 -0700

    Added some form components, properly commented todos

--- src/CustomComponents/PlayGroundCustomProps.js
+++ src/CustomComponents/PlayGroundCustomProps.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Todo: Make this work like other React components, rather than just clicking on existing ones
 export class PlayGroundCustomProps extends React.Component {
   componentDidMount = () => {
     const playGroundShowEditor = document.querySelectorAll('[title="Show editor"]');
